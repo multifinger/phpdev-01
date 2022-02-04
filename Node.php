0b240c3b73f45c84a1201e94551fae788db35545
multifinger <maksim.i.borisov@gmail.com>
2022-02-04 20:49:22 +0700

    lesson 3

---
+++ Node.php
@@ -0,0 +1,51 @@
+<?php 
+
+class Node implements NodeInterface
+{
+
+    private string $name = '';
+
+    private array $children = [];
+    
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function __toString(): string
+    {
+        return self::print($this);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    public function addChild(Node $node): self
+    {
+        $this->children[] = $node;
+
+        return $this;
+    }
+
+    private static function print(Node $node, $level = 1)
+    {
+        $html = str_repeat('+ ', $level) . $node->getName()."\n";
+
+        foreach ($node->getChildren() as $child) {
+            $html .= self::print($child, $level + 1);
+        }
+
+        return $html;
+    }
+
+}
